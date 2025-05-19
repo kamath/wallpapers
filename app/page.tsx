fb2f14ed65b9e9815d03f09d6667821d7838f2bc
Anirudh Kamath <github@kamath.io>
2025-05-19 09:28:15 -0700

    spelling

--- app/page.tsx
+++ app/page.tsx
@@ -5,26 +5,29 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center py-4 gap-12">
       <h1 className="text-2xl font-bold text-gray-800 text-center">
         Github:{" "}
-        <a href="https://github.com/kamath/wallpaper" className="text-blue-500">
+        <a
+          href="https://github.com/kamath/wallpapers"
+          className="text-blue-500"
+        >
           kamath/wallpapers
         </a>
       </h1>
       <Geometry />
-      <div className="max-w-[450px] text-center flex flex-col gap-2 text-gray-600 text-left">
+      <div className="max-w-[450px] flex flex-col gap-2 text-gray-600 text-left px-4">
         <h1 className="text-2xl font-bold text-gray-800 text-center">
           What am I looking at?
         </h1>
         <p>
-          As a math major, I took an class called Geometry, where we studied
+          As a math major, I took a class called Geometry, where we studied
           visual symmetry through the lens of abstract algebra.
         </p>
         <p>
-          In it, we learned about &quot;wallpaper groups&quot; and that there
-          are provably only 17 different ways to tile a plane, i.e. there are 17
-          different ways in which you can arrange identical shapes to completely
-          cover a plane while maintaining perfect symmetry. You can achieve
-          these patterns by applying a combination of translations, rotations,
-          and reflections to a single &quot;motif&quot;.
+          In it, we learned about the 17 &quot;wallpaper groups&quot; and that
+          there are provably only 17 different ways to tile a plane, i.e. there
+          are 17 different ways in which you can arrange identical shapes to
+          completely cover a plane while maintaining perfect symmetry. You can
+          achieve these patterns by applying a combination of translations,
+          rotations, and reflections to a single &quot;motif&quot;.
         </p>
         <p>
           I found{" "}
